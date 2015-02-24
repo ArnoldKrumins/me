bfcbffdd718b719a08e51cfd65b9ef47374dd73c
Arnold Krumins <arnold.krumins@gmail.com>
2015-02-24 22:22:35 +0100

    Initial commit

---
+++ app/directives/navmenu.js
@@ -0,0 +1,20 @@
+/**
+ * Created by arnoldkrumins on 24/02/15.
+ */
+app.directive('navMenu',function(){
+
+    return{
+
+        restrict:'AE',
+        template:'',
+        link:function(scope,element,attrs){
+
+            
+
+        }
+
+
+
+    }
+
+})
---
+++ app/module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by arnoldkrumins on 24/02/15.
+ */
+var app = angular.module('me',[]);
