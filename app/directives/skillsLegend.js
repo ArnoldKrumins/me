ebfbb7a64e4afc1bdd08ecc33c9ddf98aa785a53
Arnold Krumins <arnold.krumins@gmail.com>
2015-03-10 17:36:26 +0100

    new service for getting job data and a couple of new directives

---
+++ app/directives/skillsLegend.js
@@ -0,0 +1,16 @@
+/**
+ * Created by arnoldkrumins on 10/03/15.
+ */
+app.directive('skillsLegend',function(){
+
+    return{
+        restrict:'A',
+        replace:true,
+        scope:{},
+        template:'',
+        link:function(scope,element,attrs){
+
+
+        }
+    }
+})
---
+++ app/directives/team.js
@@ -0,0 +1,16 @@
+/**
+ * Created by arnoldkrumins on 10/03/15.
+ */
+app.directive('team',function(){
+
+    return{
+        restrict:'A',
+        replace:true,
+        scope:{},
+        template:'',
+        link:function(scope,element,attrs){
+
+
+        }
+    }
+})
