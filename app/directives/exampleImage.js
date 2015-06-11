d693d8ef28b83871e5f55fed96fd3a2caea57f00
Arnold Krumins <arnold.krumins@gmail.com>
2015-06-11 22:20:41 +0200

    removing slides and minifying

--- app/directives/exampleImage.js
+++ app/directives/exampleImage.js
@@ -7,7 +7,7 @@ app.directive('exampleImage',function(){
         restrict:'EA',
         scope:{images:'='},
         template:'<div class="example-images" ng-repeat="image in images">' +
-                        '<img ng-class="{ \'border\': image.border === true }" src="{{ image.path }}"/><span ng-hide="image.description.length === 0" class="figure-image">{{ image.figure }}</span>' +
+                        '<img ng-class="{ \'border\': image.border === true }" ng-src="image.path"/><span ng-hide="image.description.length === 0" class="figure-image">{{ image.figure }}</span>' +
                         '<p ng-hide="image.description.length === 0"><span class="figure">{{ image.figure }}).</span>{{ image.description }}</p>' +
                   '</div>'
     }
--- app/directives/skills.js
+++ app/directives/skills.js
@@ -12,43 +12,40 @@ app.directive('skills',['$interval','$rootScope',function($interval,$rootScope){
                             '<div class="skill-circle skill{{$index}}"><span>{{skill.title}}</span></div>' +
                         '</li>' +
                     '</ul>' +
-                    '<div id="desc0" class="skills-description"><span>ASP.NET</span><span>ASP.MVC</span></div>' +
-                    '<div id="desc1" class="skills-description"><span>CSS3</span><span>LESS</span><span>HTML5</span></div>' +
-                    '<div  id="desc2" class="skills-description"><span>SQL SERVER</span><span>nHibernate</span><span>Pattern Design</span></div>' +
-                    '<div  id="desc3" class="skills-description"><span>Balsamiq</span><span>Design</span></div>' +
-                    '<div  id="desc4" class="skills-description"><span>SCRUM</span><span>Kanban</span></div>' +
-                    '<div  id="desc5" class="skills-description"><span>Bootstrap</span><span>Media Queries</span></div>' +
-                    '<div  id="desc6" class="skills-description"><span>Hybrid</span><span>Xamarin</span></div>' +
-                    '<div  id="desc7" class="skills-description"><span>AngularJS</span><span>Javascipt</span><span>JQuery</span><span>SPA</span></div>' +
+//                    '<div id="desc0" class="skills-description"><span>ASP.MVC</span><i class="fa fa-circle"></i><span>ASP.NET</span><i class="fa fa-circle"></i><span>C#</span></div>' +
+//                    '<div id="desc1" class="skills-description"><span>CSS/CSS3</span><i class="fa fa-circle"><span>LESS</span><i class="fa fa-circle"><span>HTML5</span></div>' +
+//                    '<div id="desc2" class="skills-description"><span>SQL SERVER</span><i class="fa fa-circle"><span>nHibernate</span><i class="fa fa-circle"><span>Pattern Design</span></div>' +
+//                    '<div id="desc3" class="skills-description"><span>Balsamiq</span><i class="fa fa-circle"><span>UI Design</span></div>' +
+//                    '<div id="desc4" class="skills-description"><span>SCRUM</span><i class="fa fa-circle"><span>Kanban</span></div>' +
+//                    '<div id="desc5" class="skills-description"><span>Bootstrap</span><i class="fa fa-circle"><span>Media Queries</span><i class="fa fa-circle"><span>Mobile First</span></div>' +
+//                    '<div id="desc6" class="skills-description"><span>Hybrid</span><i class="fa fa-circle"><span>Xamarin</span></div>' +
+//                    '<div id="desc7" class="skills-description"><span>AngularJS</span><i class="fa fa-circle"><span>Javascipt</span><i class="fa fa-circle"><span>JQuery</span><i class="fa fa-circle"><span>SPA</span></div>' +
                   '</div>',
         link: function(scope,element,__){
 
-            $rootScope.$on('staggercomplete', function() {
-
-                var idx=0;
-                var max = scope.skills.length;
-
-                $interval(function(){
-
-
-                    TweenMax.fromTo(element.find('li')[idx],2,{scaleX:'1.3',scaleY:'1.3',delay:0.5},{scaleX:'1',scaleY:'1',ease:Back.easeOut},1);
-
-                    var tl = new TimelineMax();
-
-
-                    tl.to('#desc'+idx, 2, {autoAlpha:1})
-                    tl.to('#desc'+idx, 3, {left:"4000px"})
-                    tl.to('#desc'+idx, 0, {autoAlpha:0,left:"70px"})
-                    //tl.to('#desc'+(idx === 0 ? 0 : idx-1), 1, {autoAlpha:1})
-
-                    tl.play();
-//                    TweenMax.from('#desc'+(idx === 0 ? 0 : idx-1), 1, {autoAlpha:1})
-//                    TweenMax.from('#desc'+idx, 2, {autoAlpha:0})
-
-                    if(++idx===max){ idx=0; }
-
-                },5000)
-            })
+//            $rootScope.$on('staggercomplete', function() {
+//
+//                var idx=0;
+//                var max = scope.skills.length;
+//
+//                $interval(function(){
+//
+//
+//                    TweenMax.fromTo(element.find('li')[idx],1,{scaleX:'1.3',scaleY:'1.3',delay:0.5},{scaleX:'1',scaleY:'1',ease:Back.easeOut},1);
+//                    TweenMax.set($('#desc'+idx),0,{left:"-5000px"});
+//
+//                    var tl = new TimelineMax();
+//
+//                    tl.to($('#desc'+idx), 1, {left:"70px"})
+//                    tl.to($('#desc'+idx), 2, {autoAlpha:1})
+//                    tl.to($('#desc'+idx), 3, {left:"-5000px"})
+//
+//                    tl.play();
+//
+//                    if(++idx===max){ idx=0; }
+//
+//                },5000)
+//            })
 
         }
     }
